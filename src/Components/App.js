a064e06e4eb626a97c54c697d00a0df32260c623
smeekas <smeetapatel008@gmail.com>
2022-02-21 15:56:51 +0530

    react using webpack &  babel

---
+++ src/Components/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function App(){
+    return <p>HELLO THERE!!</p>
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.join(__dirname, "src", "index.js"),
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index_bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.js$|jsx/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
+};
